caca63be571c657372bf5dc3b4182d017d76973b
linkinlu <122452900@qq.com>
2016-11-17 16:37:54 +0800

    InitCommit

---
+++ SCSCommon/SCSCommon/Convertion/BasicConvert.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCSCommon.Convertion
+{
+    public class BasicConvert
+    {
+        public static T To<T>(object value)
+        {
+            return (T)To(value, typeof(T));
+        }
+
+        public static object To(object value, Type destinationType)
+        {
+            return To(value, destinationType, CultureInfo.InvariantCulture);
+        }
+
+        private static object To(object value, Type destinationType, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                if (destinationType.IsEnum && value is int)
+                    return Enum.ToObject(destinationType, (int)value);
+                if (!destinationType.IsInstanceOfType(value))
+                    return Convert.ChangeType(value, destinationType, culture);
+            }
+            return value;
+        }
+    }
+}
