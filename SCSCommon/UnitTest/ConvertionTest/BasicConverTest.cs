c833615fa6b0c3326922aeb67136e5d2204f8fe5
linkinlu <122452900@qq.com>
2016-11-17 16:46:06 +0800

    Add unit test project

---
+++ SCSCommon/UnitTest/ConvertionTest/BasicConverTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SCSCommon.Convertion;
+
+namespace UnitTest.ConvertionTest
+{
+    [TestFixture]
+   public class BasicConverTest
+    {
+
+        [Test]
+        public void ToTest()
+        {
+            var result = BasicConvert.To<int>("123");
+            Assert.AreEqual(result, 123);
+
+            var resultA = BasicConvert.To<int>("asd");
+            Assert.AreEqual(result, 123);
+
+        }
+    }
+}
