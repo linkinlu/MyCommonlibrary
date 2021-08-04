91eb85236eea47282ee9d37c5e53c2d998831e3e
Linkin <122452900@qq.com>
2021-08-04 11:34:32 +0800

    【添加最近的功能需求】

---
+++ SCSCommon/SCSCommon/Controller/UploadController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using SCSCommon.Extension;
+
+namespace SCSCommon.Controller
+{
+    //上传到指定服务器目录
+    public class UploadController : ApiController
+    {
+
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Upload([FromUri] string id)
+        {
+
+            HttpRequestMessage request = this.Request;
+            if (!request.Content.IsMimeMultipartContent())
+            {
+                throw new Exception("请携带附件");
+            }
+
+            
+            var root = AppConfigrationEx.GetValue<string>("ImageRoot", _ => _) ??
+                           "C:\\VueEquipment";
+                var folder = "test";
+                var outputDir = string.Join(@"\", root.Trim(), folder);
+                if (!string.IsNullOrEmpty(outputDir) && !System.IO.Directory.Exists(outputDir))
+                {
+                    System.IO.Directory.CreateDirectory(outputDir);
+                }
+
+                var provider = new FilenameMultipartFormDataStreamProvider(outputDir);
+                await request.Content.ReadAsMultipartAsync(provider);
+
+                foreach (var i in provider.FileData)
+                {
+                    {
+                        var indexOfExtension = i.LocalFileName.LastIndexOf(".", StringComparison.Ordinal);
+                        if (indexOfExtension > -1)
+                        {
+                            var fileNameIndex = i.LocalFileName.LastIndexOf("\\");
+                            var fileName = string.Empty;
+                            if (fileNameIndex > -1)
+                            {
+                                fileName = i.LocalFileName.Substring(fileNameIndex + 1,
+                                    indexOfExtension - fileNameIndex - 1);
+                            }
+
+                            var extesion = i.LocalFileName.Substring(indexOfExtension + 1);
+                            var accessory = new Accessory()
+                            {
+                                Id = Guid.NewGuid().ToString(),
+                                Name = fileName,
+                                Path = i.LocalFileName,
+                                LocalFileName = RemoveSlash(i.Headers?.ContentDisposition?.FileName)?.Trim('"'),
+                                Size = (i.Headers?.ContentDisposition?.Size) ?? request.Content.Headers.ContentLength,
+                                Extension = extesion,
+                                OwnId = id
+                            };
+
+
+                        }
+                    }
+                }
+
+
+
+
+                return Ok();
+
+        }
+
+        private string RemoveSlash(string token)
+        {
+            if (string.IsNullOrWhiteSpace(token) || !token.StartsWith("\"", StringComparison.Ordinal) || (!token.EndsWith("\"", StringComparison.Ordinal) || token.Length <= 1))
+                return token;
+            return token.Substring(1, token.Length - 2);
+
+        }
+    }
+
+    public class FilenameMultipartFormDataStreamProvider : MultipartFormDataStreamProvider
+    {
+        public FilenameMultipartFormDataStreamProvider(string path) : base(path)
+        {
+        }
+
+        public override string GetLocalFileName(System.Net.Http.Headers.HttpContentHeaders headers)
+        {
+            if (string.IsNullOrWhiteSpace(headers.ContentDisposition.FileName))
+            {
+                return string.Empty;
+            }
+
+            var indexOfFile = headers.ContentDisposition.FileName.LastIndexOf(".", StringComparison.Ordinal);
+            if (indexOfFile > 0)
+            {
+                var actualName = RemoveSlash(headers.ContentDisposition.FileName).Trim('"');
+                var fileName = actualName.Substring(0, indexOfFile - 1);
+                var extesion = actualName.Substring(indexOfFile);
+                return string.Join(".", new string[] { Guid.NewGuid().ToString(), extesion });
+
+                return string.Format("\"{0}\"", string.Join(".", new string[] { Guid.NewGuid().ToString(), extesion }));
+            }
+
+
+
+
+            return string.Empty;
+            //  return headers.ContentDisposition.FileName;
+
+            //string fileName = headers.ContentDisposition.FileName;
+            //if (string.IsNullOrWhiteSpace(fileName))
+            //{
+            //    fileName = Guid.NewGuid().ToString() + ".data";
+            //}
+            //return fileName.Replace("\"", string.Empty);
+
+
+            // return base.GetLocalFileName(headers);
+        }
+
+        private string RemoveSlash(string token)
+        {
+            if (string.IsNullOrWhiteSpace(token) || !token.StartsWith("\"", StringComparison.Ordinal) || (!token.EndsWith("\"", StringComparison.Ordinal) || token.Length <= 1))
+                return token;
+            return token.Substring(1, token.Length - 2);
+
+        }
+    }
+    public class Accessory
+
+    {
+        //public override object Id { get { return Id; } set { Id = value.ToString(); } }
+
+        public string Id { get; set; }
+
+
+        public string Name { get; set; }
+        public string LocalFileName { get; set; }
+        public long? Size { get; set; }
+        public string Extension { get; set; }
+        public string Path { get; set; }
+        public string Description { get; set; }
+        public string OwnId { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public string CreateUserId { get; set; }
+
+        public DateTime? CreateTime { get; set; }
+
+        public string LastUpdateUserId { get; set; }
+
+        public DateTime? LastUpdateTime { get; set; }
+    }
+}
--- SCSCommon/UnitTest/Consul/ConsulTest.cs
+++ SCSCommon/UnitTest/Consul/ConsulTest.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Consul;
 using NUnit.Framework;
 using SCSCommon.Consul;
 using SCSCommon.Convertion;
+using SCSCommon.QRCodeEx;
 
 namespace UnitTest.ConvertionTest
 {
@@ -17,18 +20,20 @@ namespace UnitTest.ConvertionTest
         [Test]
         public async  Task ToTest()
         {
+            //var ss1 = QRCodeGeneratorHelper.DecodeQRCode(File.ReadAllText("E:\\base64 - Copy.txt"));
+
             var allServices = await ConsulUtils.GetAllRegisterService();
-     
+
             await ConsulUtils.RegisterService("0", "MyNewService", "http://localhost:80");
             var services = await ConsulUtils.Find("MyNewService");
             await ConsulUtils.RegisterService("0", "MyNewService", "http://localhost:81");
-             services = await ConsulUtils.Find("MyNewService");
-            await ConsulUtils.RegisterService(new AgentServiceRegistration(){ID = "1" , Name = "MyService1", Address ="http://localhost:50000" });
-            await ConsulUtils.RegisterService(new AgentServiceRegistration() { ID = "2", Name = "MyService1",  Address = "http://localhost:50001" });
+            services = await ConsulUtils.Find("MyNewService");
+            await ConsulUtils.RegisterService(new AgentServiceRegistration() { ID = "1", Name = "MyService1", Address = "http://localhost:50000" });
+            await ConsulUtils.RegisterService(new AgentServiceRegistration() { ID = "2", Name = "MyService1", Address = "http://localhost:50001" });
             await ConsulUtils.RegisterService(new AgentServiceRegistration() { ID = "3", Name = "MyService1", Address = "http://localhost:50001" });
             var services2 = await ConsulUtils.Find("MyService1");
 
-            var success =await ConsulUtils.DeregisterService("2");
+            var success = await ConsulUtils.DeregisterService("2");
             var services3 = await ConsulUtils.Find("MyService1");
             var services4 = await ConsulUtils.DeregisterServices("MyService1");
 
